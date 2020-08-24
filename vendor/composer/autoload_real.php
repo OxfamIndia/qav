bc90b5cc8347ff523cc5b05ffa12d676b7293d53
rohitjha-oxfam <lalgarg1@publicisgroupe.net>
2020-08-24 18:43:19 +0530

    add files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit39564a1b8d97bf3c18ff3a6a0cfb82f0
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {

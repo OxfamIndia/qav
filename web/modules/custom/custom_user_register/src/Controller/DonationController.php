793f5d220f2da3eb17fd58384aebed948086016d
Jay Prakash Jain <Jayprakash@oxfamindia.org>
2020-09-15 15:13:31 +0530

    salesforce

--- web/modules/custom/custom_user_register/src/Controller/DonationController.php
+++ web/modules/custom/custom_user_register/src/Controller/DonationController.php
@@ -50,8 +50,8 @@ class DonationController extends ControllerBase {
 					$response = curl_exec($curl);
 					curl_close($curl);
 					$character = json_decode($response);					
-					    echo '<pre>'; print_r($character); echo '</pre>';  
-					    echo '<pre>'; print_r($data); echo '</pre>';  exit;
+					   /*  echo '<pre>'; print_r($character); echo '</pre>';  
+					    echo '<pre>'; print_r($data); echo '</pre>';  exit; */
 					  $token = $character->access_token;
 					$status = 'Unsuccessful';
 					if($data['payment_status'] == 'Success')

e94e4adaa0524ad8b87c97b71d069d2e25249782
rohitjha-oxfam <lalgarg1@publicisgroupe.net>
2020-09-18 00:06:58 +0530

    merhge

--- web/modules/custom/walk/src/Form/DayOneWalkForm.php
+++ web/modules/custom/walk/src/Form/DayOneWalkForm.php
@@ -227,7 +227,7 @@ class DayOneWalkForm extends FormBase {
     $event_name = trim($event_name, ','); */
     $event_id = $data['challenge_slot'];
     $event_data = Node::load($event_id);
-    $event_name= $event_data->get('field_event_name')->getValue(); 
+    $event_name= $event_data->get('field_event_name')->getValue()[0]['value'];
   
     $walker_full_name = $walker_name.' '.$walker_last_name;
     $walker_full_name = ucfirst($walker_full_name);

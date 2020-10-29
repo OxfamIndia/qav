09877dc79d326b0d625def4405e0a275f6384124
Devender Singh Dagar <devendersinghdagar@Devenders-MacBook-Pro.local>
2020-10-29 20:22:49 +0530

    json decode

--- web/modules/contrib/amazon_sns/src/Event/SnsNotificationSubscriber.php
+++ web/modules/contrib/amazon_sns/src/Event/SnsNotificationSubscriber.php
@@ -102,13 +102,13 @@ class SnsNotificationSubscriber implements ContainerInjectionInterface, EventSub
 
         $data = json_decode($data->additional_data);
         foreach ($data as $key => $jsons) {
-            $dataJson = json_decode($jsons);
+            //$dataJson = json_decode($jsons);
             $this->logger->info('Key received is %message. %mess2', [
               '%message' => $key,
-              '%mess2' => $dataJson,
+              '%mess2' => $jsons,
             ]);
           $this->logger->info('Value received is %message.', [
-            '%message' => $dataJson,
+            '%message' => $jsons,
           ]);
         }
         /*

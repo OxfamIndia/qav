7cd8d11714259a68715f59dac61ce1f2e131f395
Jay Prakash Jain <Jayprakash@oxfamindia.org>
2021-07-15 10:29:09 +0530

    ox

--- web/modules/custom/custom_user_register/src/Controller/DonationController.php
+++ web/modules/custom/custom_user_register/src/Controller/DonationController.php
@@ -286,8 +286,9 @@ class DonationController extends ControllerBase
     /* $domestic = $data['nationality']; */
     $node = Node::load($data['challenge_slot']);
     $eventname = "VTM-" . $data['challenge_type'] . ' ' . $node->get('title')->value;
-	//if($node->get('title')->value == 'All Slots')
-	if($node->get('title')->value == 'All slots(25feb-6Mar and 8-17 Mar)')
+	/* //if($node->get('title')->value == 'All Slots')
+	if($node->get('title')->value == 'All slots(25feb-6Mar and 8-17 Mar)') */
+	if($data['challenge_slot'] == 20)
 	{ //echo 'Jay';
 		/*  $dontate_amount_value = round($data['amount']); */
 		 $dontate_amount_value = '2000';
